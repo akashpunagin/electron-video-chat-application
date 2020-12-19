390a1ab058ef84b17b3c9b0ec98f50b5ac748638
akashpunagin <akash.punagin@gmail.com>
2020-12-19 20:53:49 +0530

    configured express server - public, views, for electron app

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@
   const PORT = process.env.PORT || 3000;
   const app = express();
   app.set('view engine', 'ejs');
-  app.use(express.static('public'));
+  app.use(express.static(path.join(__dirname + '/../public')));
+  app.set('views', path.join(__dirname, '/../views'));
   const server = http.Server(app);
   const io = socketIO(server);
   const peerServer = ExpressPeerServer(server, {debug: true});
